65725dc9d361a5c6b6843145c2592378029a430e
Tilek-Dzhenishev <tilek.dzenisev@gmail.com>
2025-07-09 13:02:23 +0200

    fix: ci error with prettier style in tsconfig.json and tsconfig.dev.json

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,10 +1,12 @@
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const importPlugin = require('eslint-plugin-import');
 
 module.exports = defineConfig([
   expoConfig,
   {
     ignores: ['dist/*'],
+    plugins: importPlugin,
     settings: {
       'import/resolver': {
         alias: {
