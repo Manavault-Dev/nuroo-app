240e16209eb6c44f405797865e073913549c8d70
Tilek-Dzhenishev <tilek.dzenisev@gmail.com>
2025-07-15 22:07:43 +0200

    chore: clean up config files before branching

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,14 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [require.resolve('babel-preset-expo')],
+    presets: ['babel-preset-expo'],
     plugins: [
       [
         'module:react-native-dotenv',
         {
           moduleName: '@env',
           path: '.env',
-          blacklist: null,
-          whitelist: null,
-          safe: false,
-          allowUndefined: true,
+          allowUndefined: false,
         },
       ],
     ],
