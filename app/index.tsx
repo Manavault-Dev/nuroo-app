c6443e7a37f469a977a00b0bdb2a8ca19cf15bdf
Tilek-Dzhenishev <tilek.dzenisev@gmail.com>
2025-09-16 18:50:17 +0200

    fix: delete unused imports and clean files

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,3 @@
-import { View } from 'react-native';
-import tw from '@/lib/design/tw';
-import { Button } from '@/components/ui/Button';
-import { Input } from '@/components/ui/Input';
 import { Redirect } from 'expo-router';
 
 export default function Index() {
